6b384e8af0cf6562f41a7ecd8df6ae3f186292f0
Abhishek Yadav <76545772+Abhishek418@users.noreply.github.com>
2023-03-25 20:22:43 +0530

    added the warning

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import expenseData from "./data/expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 import React from "react";
+import Card from "./components/UI/Card";
 
 function App() {
     const [expensesList, setExpensesList] = React.useState(expenseData);
@@ -17,6 +18,12 @@ function App() {
         <div>
             <NewExpense onAddExpense={onAddExpenseHandler} />
             <Expenses expenseData={expensesList} />
+            <Card className="expenses">
+                <h3>
+                    Please Note: No expenses are saved , once you refresh everything will be lost!. Features like authenctication , saving the
+                    expenses to the db , deleting the expenses and much more are in the development.
+                </h3>
+            </Card>
         </div>
     );
 }
